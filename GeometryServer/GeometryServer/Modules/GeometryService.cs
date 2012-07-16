2c20cfc1a49b31e7c73494f5c746b5d40c132572
Yogesh Dhanapal <geoyogesh@gmail.com>
2012-07-17 00:31:09 +0530

    Added view to geometry service and project page

---
+++ GeometryServer/GeometryServer/Modules/GeometryService.cs
@@ -0,0 +1,29 @@
+﻿using Nancy;
+
+
+namespace GeometryServer.Modules
+{
+    public class GeometryService : NancyModule
+    {
+        public GeometryService()
+            : base("/rest/services/Geometry/GeometryServer")
+        {
+            //Get["/"] = parameters =>
+            //{
+            //    return "Welcome";
+            //};
+            //Get["/{Name}"] = parameters =>
+            //{
+            //    return String.Format("Welcome {0}{1}", parameters.Name, Request.Query["Hi"]);
+            //};
+
+            //http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer
+
+            Get["/"] = parameters =>
+            {
+                return View["GeometryService"];
+            };
+
+        }
+    }
+}
